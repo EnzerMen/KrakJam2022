d2379ee713bce34acecb47802ec7719511a8404d
EnzerMen <61794871+EnzerMen@users.noreply.github.com>
2022-01-26 15:35:55 +0100

    basic prototypes and placeholders

---
+++ KrakJam2022Game/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Animator animator;
+    public float movementSpeed = 3;
+
+    private Rigidbody2D rigidbodyComponent;
+    private float horizontalInput;
+     
+    
+
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody2D>();
+        
+    }
+ 
+    void Update()
+    {
+        //zbieranie inputów do poruszania siê
+        horizontalInput = Input.GetAxis("Horizontal");
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        //poruszanie siê postaci
+
+        rigidbodyComponent.velocity = new Vector2(horizontalInput * movementSpeed, rigidbodyComponent.velocity.y);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+
+        //Rotacja horyzontalna
+
+        Vector3 characterScale = transform.localScale;
+        if (horizontalInput < 0) { characterScale.x = (float)-0.5; }
+        if (horizontalInput > 0) { characterScale.x = (float)0.5; }
+        transform.localScale = characterScale;
+    }
+
+}
