bcffa91a34670a86b6f0b797e15f5484e9d5c1d4
chomash <61794801+chomash@users.noreply.github.com>
2022-01-28 18:05:23 +0100

    Revert "klucz+portal+zjebana postac+systemkolizji+system podnnoszenia"
    This reverts commit 4d5cb62c9b5fe43c2b8bc847b4c701dce43dc005.

--- KrakJam2022Game/Assets/Scripts/Player.cs
+++ KrakJam2022Game/Assets/Scripts/Player.cs
@@ -2,53 +2,99 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Mover
+public class Player : MonoBehaviour
 {
-    [SerializeField] private Transform groundCheck = null;
-    float moveButton;
-    float jumpButton;
-    private bool canJump = true;
-    private bool isGrounded;
-    private string JUMP_TAG = "JUMPY";
 
+    public Animator animator;
+    [SerializeField] private float movementSpeed = 3;
+    [SerializeField] private float jumpPower = 3f;
+    [SerializeField] private Transform groundCheckTransform = null;
 
+    private Rigidbody2D rigidbodyComponent;
+    private float horizontalInput;
+    private bool jumpKeyWasPressed;
+     
+    
 
-
-
-    protected virtual void LateUpdate()
+    void Start()
     {
-        moveButton = Input.GetAxisRaw("Horizontal");
-        jumpButton = Input.GetAxisRaw("Jump");
+        rigidbodyComponent = GetComponent<Rigidbody2D>();
+        
+    }
+ 
+    void Update()
+    {
+        //zbieranie inputów do poruszania siê
+        horizontalInput = Input.GetAxis("Horizontal");
 
-       
-        Movement();
-        AnimationUpdate(new Vector3(moveButton, rigidBody.velocity.y, 0));
-      
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+
+        }
+
+
+        //animacje skoku
+
+        if (rigidbodyComponent.velocity.y == 0)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("isFalling", false);
+        }
+
+        if (rigidbodyComponent.velocity.y > 0)
+        {
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (rigidbodyComponent.velocity.y < 0)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("isFalling", true);
+        }
+
+
     }
 
-  /*  private void OnCollisionEnter2D(Collision collision)
+    private void FixedUpdate()
     {
-        if (collision.gameObject.CompareTag(JUMP_TAG))
+
+
+
+
+        //poruszanie siê postaci
+
+        rigidbodyComponent.velocity = new Vector2(horizontalInput * movementSpeed, rigidbodyComponent.velocity.y);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+
+        //Rotacja horyzontalna
+
+        Vector3 characterScale = transform.localScale;
+        if (horizontalInput < 0) { characterScale.x = Mathf.Abs(characterScale.x) * -1; }
+        if (horizontalInput > 0) { characterScale.x = Mathf.Abs(characterScale.x); }
+        transform.localScale = characterScale;
+
+        //skok
+
+        Debug.Log(Physics2D.OverlapCircleAll(groundCheckTransform.position, 0.1f).Length);
+
+        if (Physics2D.OverlapCircleAll(groundCheckTransform.position, 0.1f).Length == 1) 
         {
-            isGrounded = true;
-
+         
+            return;
         }
-    }*/
 
-
-    private void Movement()
-    {
-        //tmpJumpForce = jumpForce;
-        rigidBody.velocity = new Vector3(moveButton * movementSpeed, rigidBody.velocity.y, 0);
-
-
-        if (jumpButton == 1 && canJump)
+        if (jumpKeyWasPressed)
         {
-
-            rigidBody.AddForce(new Vector2(0, jumpButton*jumpForce), ForceMode2D.Impulse);
-            //tmpJumpForce = 0;
-            //canJump = false;
+            rigidbodyComponent.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            jumpKeyWasPressed = false;
         }
+
+
+
+
+
 
     }
 
