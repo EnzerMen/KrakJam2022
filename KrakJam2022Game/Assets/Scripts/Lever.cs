e65d72a0e8526660bd4fda8a573013ef1f586fb3
chomash <61794801+chomash@users.noreply.github.com>
2022-01-29 02:39:30 +0100

    lilUP for levers

--- KrakJam2022Game/Assets/Scripts/Lever.cs
+++ KrakJam2022Game/Assets/Scripts/Lever.cs
@@ -7,7 +7,9 @@ public class Lever : Collidable
     [SerializeField] private string collectedByTag = "Player";
     [SerializeField] private Movable moveThis;
     [SerializeField] private bool isToggable;
-    
+
+
+
     protected override void OnCollide(Collider2D coll)
     {
         if (coll.CompareTag(collectedByTag))
--- KrakJam2022Game/Assets/Scripts/Movable.cs
+++ KrakJam2022Game/Assets/Scripts/Movable.cs
@@ -10,8 +10,10 @@ public class Movable : MonoBehaviour
     public float forthSpeed = 3;
     public float backSpeed = 0.2f;
     private bool isMoving = false;
-    public bool standToDo = true;
     public bool canBack = false;
+    private Vector3 normalizedVector3;
+
+    private Rigidbody2D rb;
     
 
 
@@ -19,34 +21,40 @@ public class Movable : MonoBehaviour
     {
         startPos = transform.position;
         endPos = startPos + offset;
+        rb = GetComponent<Rigidbody2D>();
+        normalizedVector3 = Vector3.Normalize(endPos - startPos);
 
     }
 
 
     public void Update()
     {
+        if (Mathf.Abs(transform.position.x - endPos.x) < 0.05f && Mathf.Abs(transform.position.y - endPos.y) < 0.05f)
+        {
+            isMoving = false;
+            rb.velocity = Vector3.zero;
+        }
+
         if (isMoving)
         {
-            if (Mathf.Abs(transform.position.x - endPos.x) < 0.05f && Mathf.Abs(transform.position.y - endPos.y) < 0.05f)
-            {
-                transform.position = endPos;
-            }
-
-            transform.position = Vector3.Lerp(transform.position, endPos, forthSpeed * Time.deltaTime);
-            if (standToDo)
-            {
-                isMoving = false;
-            }
-            else if (transform.position == endPos)
-            {
-                isMoving = false;
-            }
+            rb.velocity = normalizedVector3 * forthSpeed;
 
         }
-        else if(canBack)
+        else if (canBack)
         {
-            transform.position = Vector3.Lerp(transform.position, startPos, backSpeed * Time.deltaTime);
+            if (transform.position != startPos)
+            {
+                rb.velocity = -normalizedVector3 * backSpeed;
+            }
+            else if (Mathf.Abs(transform.position.x - startPos.x) < 0.05f && Mathf.Abs(transform.position.y - startPos.y) < 0.05f)
+            {
+                isMoving = false;
+                rb.velocity = Vector3.zero;
+
+            }
+
         }
+
         
     }
 
